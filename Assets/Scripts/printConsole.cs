21ce1ef24a88b21fe8a212d83b9e45d47543dc48
Phil Wilkinson <phil@philwilky.me>
2023-03-27 14:32:53 +0100

    Example of using the data

---
+++ Assets/Scripts/printConsole.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class printConsole : MonoBehaviour
+{
+
+    public Drug drug;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Name: " + drug.name);
+        Debug.Log("Desc: " + drug.description);
+        Debug.Log("Toxicity: " + drug.toxicity);
+        Debug.Log("Stenght: " + drug.strength);
+        Debug.Log("Mix Stenghtening : " + d rug.mixStrengthening);
+        Debug.Log("Addictiveness: " + drug.addictiveness);
+        Debug.Log("Is Pure: " + drug.isPure);
+    }
+}
